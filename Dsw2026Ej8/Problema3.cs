5dbbf367748785a7fdfbe8a1ecacae6c8b569855
kira25-sys <fontsfelipe77@gmail.com>
2026-05-03 17:27:51 -0300

    ejercicio 3 resuelto se creo una variable la cual fue copiada y una clase en el mismo archivo

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Problema3
+{
+    public class Product
+    {
+        public string Description { get; set; }
+
+		public void UpdateDescritcion(string description) 
+		{
+			Description = description;
+		}
+    }
+
+    public class Comparar
+	{
+		
+		public string CompararCopias(int originalValue, Product product)
+		{
+			int original = originalValue;
+
+			
+				original++;
+
+			
+			Product copyProducto = product;
+
+			copyProducto.UpdateDescritcion("descripcion modificada");
+
+			return $"{originalValue}-{original}-{product.Description}";
+		}
+	}
+}
