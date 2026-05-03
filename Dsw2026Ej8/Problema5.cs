d570fd059ba42b171c435cff2c9bc1ce3ae44e7c
kira25-sys <fontsfelipe77@gmail.com>
2026-05-03 18:36:00 -0300

    ejercicio 5 resuelto con dos clases que sobrescriben el metodo del padre con override y virtual

---
+++ Dsw2026Ej8/Problema5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Problema5
+{
+	public class Sale
+	{
+		public decimal Valor { get; set; }
+
+		public virtual decimal CalculateTotal()
+		{
+			return Valor;
+		}
+	}
+	public class RetailSale : Sale
+	{
+		public override decimal CalculateTotal() 
+		{
+			return Valor;
+		}
+	}
+	public class WholesaleSale : Sale
+	{
+		public override decimal CalculateTotal()
+		{
+			return Valor * 0.9m;
+		}
+	}
+
+
+
+	public class Problema5
+	{
+		public decimal ObtenerImporteFinal(Sale sale)
+		{
+			return sale.CalculateTotal();
+
+		}
+
+    }
+}
