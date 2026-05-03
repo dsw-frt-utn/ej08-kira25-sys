644d408b78c3530ea5b13500791b4d3fc4b7500d
kira25-sys <fontsfelipe77@gmail.com>
2026-05-03 16:56:11 -0300

    ejercicio 2 resuelto cree un metodo publico con una clase anonima

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problema2
+{
+
+    public class CrearResumen
+    {
+        public string CreaResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total;
+
+            if (quantity <= 0)
+            {
+                total = 0;
+
+            }
+            else
+            {
+
+                total = quantity * unitPrice;
+
+            }
+
+            var anonima = new { Code = productCode, Description = productDescription, Quantity = quantity, Total = total };
+         
+            
+             
+            return $" { anonima.Code}--{ anonima.Description}--{ anonima.Total} ";
+                        
+        
+        }
+    }
+}
+
