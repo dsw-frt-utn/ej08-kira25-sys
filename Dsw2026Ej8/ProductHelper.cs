ed8a494caec2af482700fc0b248f3ff0a5c145bd
kira25-sys <fontsfelipe77@gmail.com>
2026-05-03 16:55:06 -0300

    ejercicio 1 resuelto cree una clase parcial en la cual le dividi su metodo

---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Problema1
+{
+    public partial class ProductHelper
+    {
+       
+        public const string SimboloMoneda = "C2";
+    }
+}
---
+++ Dsw2026Ej8/ProductHelper_Methods.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Problema1
+{
+
+	public partial class ProductHelper
+	{
+        public string ObtenerEtiquetaProducto(long code, string description, decimal price)
+        {
+            
+            return $"[{code}] {description} - {price:C}";
+        }
+    }
+}
