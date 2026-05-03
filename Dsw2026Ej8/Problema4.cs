2941562b10ff36b8ee612f794e202261bb723e99
kira25-sys <fontsfelipe77@gmail.com>
2026-05-03 18:05:29 -0300

    ejercicio 4 reauelto se uso un metodo para calcular el promedio que acepte valores null

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Problema4
+{
+	public class Problema4
+	{
+		public double CalcularPromedio (int? nota1, int? nota2 , int? nota3)
+		{
+			
+				double promedio = 0;
+				int cuenta = 0;
+				double acumulador = 0;
+
+				if (nota1 >= 0 && nota1 <= 10)
+				{
+					acumulador = acumulador + nota1.Value;
+					cuenta++;
+				}
+
+				if (nota2 >= 0 && nota2 <= 10)
+				{
+					acumulador = acumulador + nota2.Value;
+					cuenta++;
+				}
+
+				if (nota3 >= 0 && nota3 <= 10)
+				{
+					acumulador = acumulador + nota3.Value;
+					cuenta++;
+				}
+
+				if (cuenta == 0) 
+				{
+					return 0; 
+				}
+
+				promedio = acumulador / cuenta;
+
+				return promedio;
+			
+			
+		}
+	}
+}
